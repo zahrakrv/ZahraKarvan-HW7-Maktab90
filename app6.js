f84b0b8964f7c0a07f607594b40f9c2e474c6c52
zahraka <zahrakarvan@gmail.com>
2023-02-01 21:39:58 +0330

    edit form

--- app6.js
+++ app6.js
@@ -3,13 +3,15 @@ const firstName = document.getElementById("name");
 const midName = document.getElementById("midname");
 const lastName = document.getElementById("family");
 const course = document.getElementById("course");
-const male = document.getElementById("dot-1");
-const female = document.getElementById("dot-2");
-const other = document.getElementById("dot-3");
+const male = document.getElementById("male");
+const female = document.getElementById("female");
+const other = document.getElementById("other");
 const address = document.getElementById("address");
 const email = document.getElementById("email");
 const pass1 = document.getElementById("pass1");
 const pass2 = document.getElementById("pass2");
+const form = document.getElementById("form");
+let gender;
 
 const submit = document.getElementById("submit");
 
@@ -20,46 +22,43 @@ function saveData(e) {
   //validation
   if (firstName.value.length < 3) {
     alert("first name should have minimmum 3 charachter");
-    console.log(firstName.value);
-
     return false;
   }
-
   if (midName.value.length < 3) {
     alert("middle name should have minimmum 3 charachter");
-    console.log(midName.value);
-
     return false;
   }
   if (lastName.value.length < 3) {
     alert("last name should have minimmum 3 charachter");
-    console.log(lastName.value);
-
     return false;
   }
   if (address.value.length < 3) {
     alert("address should have minimmum 3 charachter");
-    console.log(address.value);
 
     return false;
   }
   if (email.value.length < 5) {
     alert("password should have minimmum 5 charachter");
-    console.log(email.value);
 
     return false;
   }
   if (pass1.value !== pass2.value) {
     alert("your password have not match");
-    console.log(pass2.value);
-
-    console.log(pass1.value);
 
     return false;
   } else {
     alert("your form send successfully!");
-    return true;
+    // return true;
   }
+  // console.log(firstName);
+  console.log(firstName.value);
+  console.log(midName.value);
+  console.log(lastName.value);
+  console.log(address.value);
+  console.log(email.value);
+  console.log(pass2.value);
+  console.log(pass1.value);
+  return true;
 }
 ///required errors
 let a = 0;
