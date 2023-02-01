f70b4d6659fe842553535b676da969f35ca59591
zahraka <zahrakarvan@gmail.com>
2023-02-01 20:11:18 +0330

    add html and css files of question 3

---
+++ app3.js
@@ -0,0 +1,26 @@
+// const arr = [
+//   "Hare",
+//   "Krishna",
+//   "Hare",
+//   "Krishna",
+//   "Krishna",
+//   "Krishna",
+//   "Hare",
+//   "Hare",
+//   ":-O",
+// ];
+function unique() {
+  const values = new Set([
+    "Hare",
+    "Krishna",
+    "Hare",
+    "Krishna",
+    "Krishna",
+    "Krishna",
+    "Hare",
+    "Hare",
+    ":-O",
+  ]);
+  console.log(values);
+}
+unique();
